0be10833949e5e0e437b0e26602211c87a444a3e
ptr1337 <admin@ptr1337.dev>
2021-07-15 20:30:43 +0000

    „plasma/packages.sh“ ändern

--- plasma/packages.sh
+++ plasma/packages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-asp export bluedevil breeze breeze-gtk discover drkonqi kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kscreen kwayland-integration kwayland-server kwin libkscreen plasma-desktop plasma-firewall plasma-pa plama-integration plasma-workspace
+asp export oxygen-icons sonnet solid syntax-highlighting plasma-framework kwindowsystem kwayland frameworkintegration extra-cmake-modules latte-dock kdoctools gpsd baloo networkmanager-qt plasma-wayland-protocols kunitconversion kinit knotifyconfig ksystemstats ktexteditor libqalculate kde-cli-tools appstream-qt xorg-xrdb xorg-xsetroot kactivitymanagerd kholidays xorg-xmessage milou prison kwin plasma-integration kpeople kactivities-stats libkscreen kquickcharts kuserfeedback kio-extras kio-fuse baloo kwayland-integration xorg-xwayland gawk kdelibs4support polkit-kde-agent libxkbfile kmenuedit systemsettings baloo accountsservice xdg-user-dirs packagekit-qt5 kcmutils qt5-tools krunner kscreenlocker kactivities kdeclarative kwayland-server kirigami2 qt5-wayland kate plasma-desktop plasma-workspace konsole ark mesa lib32-mesa zstd lib32-zstd
 
 find . -name "PKGBUILD" | xargs -I {} sed -i 's/arch=(x86_64)/arch=(x86_64_v3)/' {}
+
+#find . -name "PKGBUILD" | xargs -I {} sed -i 's/arch=('x86_64')/arch=('x86_64_v3')/' {}
