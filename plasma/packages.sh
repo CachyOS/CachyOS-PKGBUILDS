99f5057d4da52b156084781dbf5460911fa63cb5
ptr1337 <admin@ptr1337.dev>
2021-07-16 18:05:35 +0000

    „plasma/packages.sh“ ändern

--- plasma/packages.sh
+++ plasma/packages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-asp export oxygen-icons sonnet solid syntax-highlighting plasma-framework kwindowsystem kwayland frameworkintegration extra-cmake-modules latte-dock kdoctools gpsd baloo networkmanager-qt plasma-wayland-protocols kunitconversion kinit knotifyconfig ksystemstats ktexteditor libqalculate kde-cli-tools appstream-qt xorg-xrdb xorg-xsetroot kactivitymanagerd kholidays xorg-xmessage milou prison kwin plasma-integration kpeople kactivities-stats libkscreen kquickcharts kuserfeedback kio-extras kio-fuse baloo kwayland-integration xorg-xwayland gawk kdelibs4support polkit-kde-agent libxkbfile kmenuedit systemsettings baloo accountsservice xdg-user-dirs packagekit-qt5 kcmutils qt5-tools krunner kscreenlocker kactivities kdeclarative kwayland-server kirigami2 qt5-wayland kate plasma-desktop plasma-workspace konsole ark mesa lib32-mesa zstd lib32-zstd
+asp export oxygen-icons sonnet solid syntax-highlighting plasma-framework kwindowsystem kwayland frameworkintegration extra-cmake-modules latte-dock kdoctools gpsd baloo networkmanager-qt plasma-wayland-protocols kunitconversion kinit knotifyconfig ksystemstats ktexteditor libqalculate kde-cli-tools appstream-qt xorg-xrdb xorg-xsetroot kactivitymanagerd kholidays xorg-xmessage milou prison kwin plasma-integration kpeople kactivities-stats libkscreen kquickcharts kuserfeedback kio-extras kio-fuse baloo kwayland-integration xorg-xwayland gawk kdelibs4support polkit-kde-agent libxkbfile kmenuedit systemsettings baloo accountsservice xdg-user-dirs packagekit-qt5 kcmutils qt5-tools krunner kscreenlocker kactivities kdeclarative kwayland-server kirigami2 qt5-wayland kate plasma-desktop plasma-workspace konsole ark mesa lib32-mesa zstd lib32-zstd ffmpeg firefox
+
+paru -G discord-canary lightcord-bin vscodium qownnotes  kotatogram-desktop 
 
 find . -name "PKGBUILD" | xargs -I {} sed -i 's/arch=(x86_64)/arch=(x86_64_v3)/' {}
 
@@ -12,8 +14,8 @@ files=$(find . -name "PKGBUILD")
 for f in $files
 do
 	d=$(dirname $f)
-	echo "makepkg -sr --sign $f"
+	echo "makepkg -sr --sign --skipinteg --rmdeps $f"
 	cd $d
-	makepkg -sr --sign $d/PKGBUILD
+	makepkg -sr --sign --skipinteg --rmdeps $d/PKGBUILD
 	cd ..
 done
