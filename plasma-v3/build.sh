fe5183832856f62656abc0f3e219de01ae675678
ptr1337 <admin@ptr1337.dev>
2021-07-21 18:41:36 +0000

    Dateien hochladen nach „plasma-v3“

--- plasma-v3/build.sh
+++ plasma-v3/build.sh
@@ -4,17 +4,29 @@
 #  ninja cmake libffi libedit ncurses libxml2 \
 #  ocaml ocaml-ctypes ocaml-findlib python-setuptools \
 #  python-psutil python-sphinx python-recommonmark
-  
- ## only needed if not already built 
+
+ ## only needed if not already built
 # git clone https://github.com/h0tc0d3/llvm-git.git && cd llvm-# # git && makepkg -cfi --noconfirm
 
-asp export alsa-lib dolphin gwenview syntax-highlighting plasma-framework kwindowsystem kwayland frameworkintegration latte-dock kinit knotifyconfig xorg-xrdb xorg-xsetroot xorg-xmessage kwin plasma-integration  libkscreen kwayland-integration xorg-xwayland kmenuedit systemsettings qt5-tools krunner kscreenlocker kactivities kdeclarative kwayland-server kirigami2 qt5-wayland wayland-protocols kate wayland plasma-desktop plasma-workspace plasma-wayland-protocol konsole ark mesa lib32-mesa zstd lib32-zstd libx11 openssl pulseaudio xorg-server  egl-wayland dbus  alsa-plugins  libxcomposite mpv ffmpeg libglvnd
+asp export $(pacman -Sg plasma)
+asp export $(pacman -Sg kde-network)
+asp export $(pacman -Sg kde-accessibility)
+asp export $(pacman -Sg kde-system)
+asp export $(pacman -Sg kde-graphics)
+asp export $(pacman -Sg kde-utilities)
+asp export $(pacman -Sg kde-applications)
+asp export $(pacman -Sg kf5)
+asp export $(pacman -Sg qt5)
+asp export $(pacman -Sg xorg)
+
 
 find . -name "PKGBUILD" | xargs -I {} sed -i 's/arch=(x86_64)/arch=(x86_64_v3)/' {}
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/arch=('x86_64')/arch=('x86_64_v3')/" {}
 
 find . -name "PKGBUILD" | xargs -I {} sed -i "s/pkgname=/export CC=clang\\nexport CXX=clang++\\nexport LD=ld.lld\\nexport CC_LD=lld\\nexport CXX_LD=lld\\nexport AR=llvm-ar\\nexport NM=llvm-nm\\nexport STRIP=llvm-strip\\nexport OBJCOPY=llvm-objcopy\\nexport OBJDUMP=llvm-objdump\\nexport READELF=llvm-readelf\\nexport RANLIB=llvm-ranlib\\nexport HOSTCC=clang\\nexport HOSTCXX=clang++\\nexport HOSTAR=llvm-ar\\nexport HOSTLD=ld.lld\\npkgname=/" {}
+
+#find . -name "PKGBUILD" | xargs -I {} sed -i 's/cmake=/export CFLAGS="${CFLAGS} -flto=thin"\\nexport CXXFLAGS="${CXXFLAGS} -flto=thin"\\ncmake=/' {}
 
 files=$(find . -name "PKGBUILD")
 
@@ -32,9 +44,9 @@ files=$(find . -name "PKGBUILD")
 
 for f in $files
 do
-	d=$(dirname $f)
-	echo "makepkg -sr --sign --skipinteg --noconfirm $f"
-	cd $d
-	makepkg -sr --sign --skipinteg --noconfirm $d/PKGBUILD
-	cd ..
+        d=$(dirname $f)
+        echo "makepkg -sr --sign --skipinteg --noconfirm $f"
+        cd $d
+        makepkg -sr --sign --skipinteg --noconfirm $d/PKGBUILD
+        cd ..
 done
