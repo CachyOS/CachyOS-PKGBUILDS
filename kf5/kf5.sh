0279b3b6d6969c987a5948d6c69c5980e2981e78
ptr1337 <admin@ptr1337.dev>
2021-08-07 22:00:45 +0200

    update
    Former-commit-id: 90e36a47d7934f4fda6fe89c3482b833c2c48cfc

--- kf5/kf5.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-asp export oxygen-icons sonnet solid syntax-highlighting plasma-framework kwindowsystem kwayland frameworkintegration extra-cmake-modules latte-dock
-
-find . -name "PKGBUILD" | xargs -I {} sed -i 's/arch=(x86_64)/arch=(x86_64_v3)/' {}
